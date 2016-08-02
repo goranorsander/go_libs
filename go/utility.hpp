9ec6e0404cc9e9b2f9eb1b67af654d960791ac9e
Göran Orsander <goran.orsander@gmail.com>
2016-08-02 10:15:58 +0200

    Adding utility project

---
+++ go/utility.hpp
@@ -0,0 +1,18 @@
+#ifndef GO_UTILITY_HPP_INCLUDED
+#define GO_UTILITY_HPP_INCLUDED
+
+//
+//  utility.hpp
+//
+//  Copyright 2015-2016 Göran Orsander
+//
+//  This file is part of the GO.libraries.
+//  Distributed under the GO Software License, Version 1.0.
+//  See accompanying file LICENSE_1_0.txt.
+//
+
+#include <go/utility/sanity.hpp>
+
+#include <go/utility/iterator/forward_iterator.hpp>
+
+#endif  // #ifndef GO_UTILITY_HPP_INCLUDED
