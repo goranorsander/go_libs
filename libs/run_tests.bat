cca7430d4ec2e974670366f0430dfa0d3f7ceca5
Göran Orsander <goran.orsander@gmail.com>
2017-09-21 10:39:40 +0200

    Adding script to run all unit tests

---
+++ libs/run_tests.bat
@@ -0,0 +1,6 @@
+.\GO_libraries_boost_mvvm_tests.exe --gtest_output=xml:GO_libraries_boost_mvvm_tests_report.xml
+.\GO_libraries_boost_property_tests.exe --gtest_output=xml:GO_libraries_boost_property_tests_report.xml
+.\GO_libraries_std_mvvm_tests.exe --gtest_output=xml:GO_libraries_std_mvvm_tests_report.xml
+.\GO_libraries_std_property_tests.exe --gtest_output=xml:GO_libraries_std_property_tests_report.xml
+.\GO_libraries_std_signals_tests.exe --gtest_output=xml:GO_libraries_std_signals_tests_report.xml
+.\GO_libraries_std_utility_tests.exe --gtest_output=xml:GO_libraries_std_utility_tests_report.xml
