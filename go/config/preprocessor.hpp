64ee8d87002be80f63406194f52d19b200df9714
Göran Orsander <goran.orsander@gmail.com>
2018-01-14 18:04:48 +0100

    Adding go/config/preprocessor.hpp

---
+++ go/config/preprocessor.hpp
@@ -0,0 +1,17 @@
+#ifndef GO_CONFIG_PREPROCESSOR_HPP_INCLUDED
+#define GO_CONFIG_PREPROCESSOR_HPP_INCLUDED
+
+//
+//  preprocessor.hpp
+//
+//  Copyright 2017 Göran Orsander
+//
+//  This file is part of the GO.libraries.
+//  Distributed under the GO Software License, Version 2.0.
+//  See accompanying file LICENSE.md.
+//
+
+#define GO_ADD_QUOTES_HELPER(s) #s
+#define GO_ADD_QUOTES(s) GO_ADD_QUOTES_HELPER(s)
+
+#endif  // #ifndef GO_CONFIG_PREPROCESSOR_HPP_INCLUDED
