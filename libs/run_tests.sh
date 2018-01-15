3ef3e03a0d57d02ce469a8df6295b52ec934612f
Göran Orsander <goran.orsander@gmail.com>
2018-01-15 20:03:56 +0100

    New test suites

--- libs/run_tests.sh
+++ libs/run_tests.sh
@@ -2,6 +2,9 @@
 
 ./GO_libraries_boost_mvvm_tests --gtest_output=xml:GO_libraries_boost_mvvm_tests_report.xml
 ./GO_libraries_boost_property_tests --gtest_output=xml:GO_libraries_boost_property_tests_report.xml
+./GO_libraries_boost_utility_tests --gtest_output=xml:GO_libraries_boost_utility_tests_report.xml
+./GO_libraries_std_config_tests --gtest_output=xml:GO_libraries_std_config_tests_report.xml
+./GO_libraries_std_exception_tests --gtest_output=xml:GO_libraries_std_exception_tests_report.xml
 ./GO_libraries_std_mvvm_tests --gtest_output=xml:GO_libraries_std_mvvm_tests_report.xml
 ./GO_libraries_std_property_tests --gtest_output=xml:GO_libraries_std_property_tests_report.xml
 ./GO_libraries_std_signals_tests --gtest_output=xml:GO_libraries_std_signals_tests_report.xml
