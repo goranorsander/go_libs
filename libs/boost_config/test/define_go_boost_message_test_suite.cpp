78214089b8bfcd34ad3e1f9546145960f5880508
Göran Orsander <goran.orsander@gmail.com>
2018-01-29 19:34:06 +0100

    Added unit tests for GO.Boost.Config and GO.Boost.Exceptions

---
+++ libs/boost_config/test/define_go_boost_message_test_suite.cpp
@@ -0,0 +1,24 @@
+//
+//  define_go_boost_message_test_suite.cpp
+//
+//  Copyright 2018 Göran Orsander
+//
+//  This file is part of the GO.libraries.
+//  Distributed under the GO Software License, Version 2.0.
+//  See accompanying file LICENSE.md.
+//
+
+#include <gtest/gtest.h>
+#include <go_boost/config.hpp>
+
+namespace
+{
+
+TEST(boost_define_go_boost_message_test_suite, test_define_go_boost_message)
+{
+    #if !defined(GO_BOOST_MESSAGE)
+    EXPECT_TRUE(false);
+    #endif
+}
+
+}
---
+++ libs/boost_config/test/define_go_boost_typename_test_suite.cpp
@@ -0,0 +1,34 @@
+//
+//  define_go_boost_typename_test_suite.cpp
+//
+//  Copyright 2018 Göran Orsander
+//
+//  This file is part of the GO.libraries.
+//  Distributed under the GO Software License, Version 2.0.
+//  See accompanying file LICENSE.md.
+//
+
+#include <gtest/gtest.h>
+#include <go_boost/config.hpp>
+#include <boost/preprocessor/stringize.hpp>
+
+namespace
+{
+
+TEST(boost_define_go_boost_typename_test_suite, test_define_go_boost_typename)
+{
+    #if defined(GO_BOOST_TYPENAME)
+    #if defined(GO_BOOST_TYPENAME_REQUIRED)
+    const std::string expected_define("typename");
+    const std::string go_boost_typename(BOOST_PP_STRINGIZE(GO_BOOST_TYPENAME));
+    #else
+    const std::string expected_define("");
+    const std::string go_boost_typename("");
+    #endif
+    EXPECT_EQ(expected_define, go_boost_typename);
+    #else
+    EXPECT_TRUE(false);
+    #endif
+}
+
+}
