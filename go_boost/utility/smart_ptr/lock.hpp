a26c137ef04309c4b7454a75a74d11d3c51fe0ad
Göran Orsander <goran.orsander@gmail.com>
2020-07-20 20:52:06 +0200

    Add utility/smart_ptr/lock.hpp

---
+++ go_boost/utility/smart_ptr/lock.hpp
@@ -0,0 +1,46 @@
+#ifndef GO_BOOST_UTILITY_SMART_PTR_LOCK_HPP_INCLUDED
+#define GO_BOOST_UTILITY_SMART_PTR_LOCK_HPP_INCLUDED
+
+//
+//  lock.hpp
+//
+//  Copyright 2020 Göran Orsander
+//
+//  This file is part of the GO.libraries.
+//  Distributed under the GO Software License, Version 2.0.
+//  See accompanying file LICENSE.md.
+//
+
+#include <go_boost/config.hpp>
+
+#ifdef BOOST_HAS_PRAGMA_ONCE
+#pragma once
+#endif  // #ifdef BOOST_HAS_PRAGMA_ONCE
+
+#include <boost/shared_ptr.hpp>
+#include <boost/weak_ptr.hpp>
+
+namespace go_boost
+{
+namespace utility
+{
+namespace smart_ptr
+{
+
+template<typename T>
+inline boost::shared_ptr<T> lock(const boost::shared_ptr<T>& sp)
+{
+    return sp;
+}
+
+template<typename T>
+inline boost::shared_ptr<T> lock(const boost::weak_ptr<T>& wp)
+{
+    return wp.lock();
+}
+
+}
+}
+}
+
+#endif  // #ifndef GO_BOOST_UTILITY_SMART_PTR_LOCK_HPP_INCLUDED
