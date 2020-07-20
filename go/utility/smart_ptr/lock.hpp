9983f1c4599c2932c706540cfc2a8a37a7dc07f9
Göran Orsander <goran.orsander@gmail.com>
2020-07-20 21:09:25 +0200

    Add utility/smart_ptr/lock.hpp

---
+++ go/utility/smart_ptr/lock.hpp
@@ -0,0 +1,45 @@
+#ifndef GO_UTILITY_SMART_PTR_LOCK_HPP_INCLUDED
+#define GO_UTILITY_SMART_PTR_LOCK_HPP_INCLUDED
+
+//
+//  lock.hpp
+//
+//  Copyright 2020 Göran Orsander
+//
+//  This file is part of the GO.libraries.
+//  Distributed under the GO Software License, Version 2.0.
+//  See accompanying file LICENSE.md.
+//
+
+#include <go/config.hpp>
+
+#ifdef HAS_PRAGMA_ONCE
+#pragma once
+#endif  // #ifdef HAS_PRAGMA_ONCE
+
+#include <memory>
+
+namespace go
+{
+namespace utility
+{
+namespace smart_ptr
+{
+
+template<typename T>
+inline std::shared_ptr<T> lock(const std::shared_ptr<T>& sp)
+{
+    return sp;
+}
+
+template<typename T>
+inline std::shared_ptr<T> lock(const std::weak_ptr<T>& wp)
+{
+    return wp.lock();
+}
+
+}
+}
+}
+
+#endif  // #ifndef GO_UTILITY_SMART_PTR_LOCK_HPP_INCLUDED
